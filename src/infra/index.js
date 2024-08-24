67d38dc8f67d5973cd48014aae90217370d8bb05
goodtobebad <49612579+goodtobebad@users.noreply.github.com>
2024-08-25 00:41:40 +0200

    Update index.js

--- src/infra/index.js
+++ src/infra/index.js
@@ -1,2 +1,2 @@
 //export const __DOMAIN__ = "http://localhost:8080"
-export const __DOMAIN__ = "dominos-production.up.railway.app:8080"
+export const __DOMAIN__ = "https://dominos-production.up.railway.app:8080"
